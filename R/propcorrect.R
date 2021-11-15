348ca452d5137d28c4a5b785b762a1cc4709b611
Abbey Camaclang <abbey.camaclang@gmail.com>
2021-11-14 20:04:24 -0800

    reverting back to earlier version

--- R/propcorrect.R
+++ R/propcorrect.R
@@ -9,19 +9,21 @@
 #' @aliases propsig
 #'
 #' @param B The threshold false positive rate \eqn{\beta}.
-#' @param t The amount of time to spend learning.
-#' @param a The accuracy of identification at time t based on the learning curve used.
-#' @param x A positive value for the rate of loss (if \code{q = 1}) or growth (if \code{q = -1}).
-#' @param b,m Parameters defining the shape of the learning curves; if
-#'   manually specified, overrides the default parameters.
+#' @param yint The accuracy (\code{a}) of classification or identification at
+#'   time \code{t = 0} (i.e., the y-intercept of the learning curve); default
+#'   value \code{yint = 1}.
 #' @param q A value indicating whether optimal time should be calculated based
-#'   on loss model (\code{q = 1}) or growth model (\code{q = -1}).
+#'   on a loss model (\code{q = 1}) or growth model (\code{q = -1}).
+#' @param x A positive value for the rate of loss (if \code{q = 1}) or growth
+#'   (if \code{q = -1}).
+#' @param b,m Parameters defining the shape of the learning curves; if manually
+#'   specified, overrides the default parameters.
 #' @name propcorrect
 #'
 #' @examples
-#' proplin(B = 0.2, m = 0.2, a = 4, t = 3, x = 0.1)
-#' prophyp(B = 0.5, b = 10, m = 5, a = 2, t = 5, x = 0.01)
-#' propsig(B = 0.2, b = 9, m = 0.2, a = 6, t = 2, x = 0.5)
+#' proplin(B = 0.2, x = 5)
+#' prophyp(B = 0.5, hyp = 2, x = 1)
+#' propsig(B = 0.2, b = 9, m = 0.2, x = 0.5)
 #'
 #' @return The proportion correctly identified at the optimal time given the
 #'   rate of loss (or growth) \code{x}.
@@ -29,31 +31,36 @@ NULL
 
 #' @rdname propcorrect
 #' @export
-proplin <- function(B, m = 9/50, a, t, q = 1, x) {
+proplin <- function(B, m = (9/50), yint = 1, q = 1, x) {
   if(B <= 0 || B >= 1) stop("B must be between 0 and 1")
   if(m == 9/50) message("Default linear function parameters used")
-  
   if(x < -m*log(B)) {
+    t <- optlin(B = B, m = m, yint = yint, q = 1, x = x)
+    a <- linear(m = m, yint = yint, x = t)
     exp(-(q*x) * t) * B^(1/a)
   } else (B)
 }
+
 #' @rdname propcorrect
 #' @export
-prophyp <- function(B, b = 9.25, m = 1, a, t, q = 1, x) {
+prophyp <- function(B, b = 9.25, m = 1, yint = 1, q = 1, x) {
   if(B <= 0 || B >= 1) stop("B must be between 0 and 1")
   if(b == 9.25 || m == 1) message("Default hyperbolic curve parameters used")
-
   if(x < -(b/m)*log(B)) {
+    t <- opthyp(B = B, b = b, m = m, yint = yint, q = 1, x = x)
+    a <- hyperb(b = b, m = m, yint = yint, x = t)
     exp(-(q*x) * t) * B^(1/a)
   } else (B)
 }
+
 #' @rdname propcorrect
 #' @export
-propsig <- function(B, b = 10, m = 0.15, a, t, q = 1, x) {
+propsig <- function(B, b = 10, m = 0.15, yint = 1, q = 1, x) {
   if(B <= 0 || B >= 1) stop("B must be between 0 and 1")
   if(b == 10 || m == 0.15) message("Default sigmoid curve parameters used")
-
   if (x < (-((b-1)*m)/b)*log(B)) {
+    t <- optsig(B = B, b = b, m = m, yint = yint, q = q, x = x)
+    a <- sigmoid(b = b, m = m, yint = yint, x = t)
     exp(-(q*x) * t) * B^(1/a)
   } else (B)
 }
